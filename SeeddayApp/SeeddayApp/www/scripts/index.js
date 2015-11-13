39aa570c31e5b435c80637242ea53f757a2fc906
yoonsuk <hannut91@gmail.com>
2015-11-13 22:46:31 +0900

    스플래시 기능이랑 이미지 넣음

--- SeeddayApp/SeeddayApp/www/scripts/index.js
+++ SeeddayApp/SeeddayApp/www/scripts/index.js
@@ -8,11 +8,13 @@
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
     function onDeviceReady() {
+        navigator.splashscreen.show();
+ 
         // Cordova 일시 중지를 처리하고 이벤트를 다시 시작합니다.
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
         
-        // TODO: Cordova가 로드되었습니다. 여기서 Cordova가 필요한 모든 설치를 수행합니다.
+        // TODO: Cordova가 로드되었습니다. 여기서 Cordova가 필요한 모든 설치를 수행합니다._
     };
 
     function onPause() {
