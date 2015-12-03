436dcdcb683c7a421bf3d106b960eada0ffc74b2
yoonsuk <hannut91@gmail.com>
2015-12-03 14:01:33 +0900

    데이터베이스로 변경

--- SeeddayApp/SeeddayApp/www/scripts/index.js
+++ SeeddayApp/SeeddayApp/www/scripts/index.js
@@ -7,11 +7,15 @@
     */
     document.addEventListener('deviceready', onDeviceReady.bind(this), false);
     function onDeviceReady() {
+        /*
         window.sqlitePlugin.openDatabase({ name: "seed.db" }, function (db) {
             db.transaction(function (tx) {
-                tx.executeSql('CREATE TABLE IF NOT EXISTS seedday (id integer primary key, goal text, curDate text)');
+                tx.executeSql('CREATE TABLE IF NOT EXISTS seedday (id integer primary key, goal text, curDate text, Day1 text, Day2 text, Day3 text)');
             });
         });
+        */
+        var db = window.sqlitePlugin.openDatabase({ name: "seed.db" });
+        db.executeSql("CREATE TABLE SEEDDAY (ID INTEGER PRIMARY KEY AUTOINCREMENT, GOAL TEXT, CURDATE TEST, DAY1 TEXT, DAY2 TEXT, DAY3 TEXT,PLANTNUMBER INTEGER)");
         navigator.splashscreen.show();
         document.addEventListener('pause', onPause, false);
         document.addEventListener('resume', onResume, false);
