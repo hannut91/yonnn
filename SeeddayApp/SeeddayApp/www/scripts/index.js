77dc69bc683d020125d749b2000d770f688c0f87
yoonsuk <hannut91@gmail.com>
2015-12-07 14:38:15 +0900

    앵귤러 변경 전

--- SeeddayApp/SeeddayApp/www/scripts/index.js
+++ SeeddayApp/SeeddayApp/www/scripts/index.js
@@ -1,19 +1,9 @@
 (function () {
     "use strict";
-    /*
-    if (localStorage.getItem('goal') != null) {
-        location = "second.html";
-    }
-    */
+
     document.addEventListener('deviceready', onDeviceReady.bind(this), false);
     function onDeviceReady() {
-        /*
-        window.sqlitePlugin.openDatabase({ name: "seed.db" }, function (db) {
-            db.transaction(function (tx) {
-                tx.executeSql('CREATE TABLE IF NOT EXISTS seedday (id integer primary key, goal text, curDate text, Day1 text, Day2 text, Day3 text)');
-            });
-        });
-        */
+
         var db = window.sqlitePlugin.openDatabase({ name: "seed.db" });
         db.executeSql("CREATE TABLE SEEDDAY (ID INTEGER PRIMARY KEY AUTOINCREMENT, GOAL TEXT, CURDATE TEST, DAY1 TEXT, DAY2 TEXT, DAY3 TEXT,PLANTNUMBER INTEGER)");
         navigator.splashscreen.show();
