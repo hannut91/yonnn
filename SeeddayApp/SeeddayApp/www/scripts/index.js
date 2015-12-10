c065e6f825573e0dd2eb6b7f072a152a0122c19d
yoonsuk <hannut91@gmail.com>
2015-12-10 12:19:16 +0900

    ddd

--- SeeddayApp/SeeddayApp/www/scripts/index.js
+++ SeeddayApp/SeeddayApp/www/scripts/index.js
@@ -5,7 +5,10 @@
     function onDeviceReady() {
 
         var db = window.sqlitePlugin.openDatabase({ name: "seed.db" });
-        db.executeSql("CREATE TABLE SEEDDAY (ID INTEGER PRIMARY KEY AUTOINCREMENT, GOAL TEXT, CURDATE TEST, DAY1 TEXT, DAY2 TEXT, DAY3 TEXT,PLANTNUMBER INTEGER)");
+        db.executeSql("CREATE TABLE IF NOT EXISTS SEEDDAY (ID INTEGER PRIMARY KEY AUTOINCREMENT, GOAL TEXT, CURDATE TEST, DAY1 TEXT, DAY2 TEXT, DAY3 TEXT,PLANTNUMBER INTEGER, COMPLETE TEXT)");
+        db.executeSql("SELECT *, MAX(ID) from seedday", [], function (res) {
+            if (res.rows.item(0).COMPLETE == "notcomplete") location = "second.html";
+        });
         navigator.splashscreen.show();
         document.addEventListener('pause', onPause, false);
         document.addEventListener('resume', onResume, false);
