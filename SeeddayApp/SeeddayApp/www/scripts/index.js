9ae0e2aff403394f6800b649e9cd82c0c9d83bbd
yoonsuk <hannut91@gmail.com>
2015-12-12 17:19:35 +0900

    씨드데이 기능 완성 (디자인 완성 X)

--- SeeddayApp/SeeddayApp/www/scripts/index.js
+++ SeeddayApp/SeeddayApp/www/scripts/index.js
@@ -3,16 +3,19 @@
 
     document.addEventListener('deviceready', onDeviceReady.bind(this), false);
     function onDeviceReady() {
-
-        var db = window.sqlitePlugin.openDatabase({ name: "seed.db" });
-        db.executeSql("CREATE TABLE IF NOT EXISTS SEEDDAY (ID INTEGER PRIMARY KEY AUTOINCREMENT, GOAL TEXT, CURDATE TEST, DAY1 TEXT, DAY2 TEXT, DAY3 TEXT,PLANTNUMBER INTEGER, COMPLETE TEXT)");
-        db.executeSql("SELECT *, MAX(ID) from seedday", [], function (res) {
-            if (res.rows.item(0).COMPLETE == "notcomplete") location = "second.html";
-        });
-        navigator.splashscreen.show();
         document.addEventListener('pause', onPause, false);
         document.addEventListener('resume', onResume, false);
-        
+        var db = window.sqlitePlugin.openDatabase({ name: "seed.db" });
+        db.executeSql("CREATE TABLE IF NOT EXISTS SEEDDAY (ID INTEGER PRIMARY KEY AUTOINCREMENT, GOAL TEXT, CURDATE TEST, DAY1 TEXT, DAY2 TEXT, DAY3 TEXT,PLANTNUMBER INTEGER, COMPLETE TEXT)");
+        db.executeSql("SELECT *, MAX(ID) from seedday",[],function(res){
+            if (res.rows.item(0).COMPLETE == "notcomplete") {
+                location = "second.html";
+                navigator.splashscreen.show();
+            }
+            else{
+                navigator.splashscreen.show();
+            }
+        }); 
     };
     function onPause() { };
     function onResume() { };
